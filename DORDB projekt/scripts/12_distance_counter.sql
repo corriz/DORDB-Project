6b6e481bc09fd07659cc9927960d4a3b93b37ae5
Pavel Pech <vipper@seznam.cz>
2013-06-23 16:42:13 +0200

    oprava procedury - secteni vzdalenosti

--- DORDB projekt/scripts/12_distance_counter.sql
+++ DORDB projekt/scripts/12_distance_counter.sql
@@ -1,6 +1,6 @@
 ---- PRIDA SLOUPEC
 ALTER TABLE RIDICI ADD (PRUMER_VZDALENOST NUMBER(10));
-
+/
 ----PROCEDURA
 CREATE OR REPLACE PROCEDURE AVR_DISTANCE_COUNTER (JID in number) AS 
 
@@ -18,12 +18,12 @@ BEGIN
   j.vypujceni_id = vypujceni.vypujcka_id
   where VYPUJCENI.RIDIC_ID = RID;
   
-  SELECT v.vzdalenost_celkem INTO CELKEM FROM vypujceni v WHERE v.ridic_id = RID;
+  SELECT sum(v.vzdalenost_celkem) INTO CELKEM FROM vypujceni v WHERE v.ridic_id = RID;
   
   PRUMER := CELKEM / POCET_JIZD;
   UPDATE RIDICI SET PRUMER_VZDALENOST = PRUMER WHERE RIDICI.RIDIC_ID = RID;
 END;
-
+/
 ----TRIGGER
 CREATE OR REPLACE TRIGGER SET_AVR_DRIVER_DISTANCE 
 AFTER INSERT OR DELETE OR UPDATE ON JIZDY
@@ -31,6 +31,10 @@ for each row
 BEGIN
   AVR_DISTANCE_COUNTER(:NEW.jizda_id);
 END;
-
-
-
+/
+SELECT v.ridic_id from vypujceni v inner join jizdy j 
+  on v.vypujcka_id = j.vypujceni_id 
+  where j.jizda_id = 15; 
+SELECT COUNT(j.jizda_id) FROM JIZDY j INNER JOIN VYPUJCENI ON
+  j.vypujceni_id = vypujceni.vypujcka_id
+  where VYPUJCENI.RIDIC_ID = 15;
